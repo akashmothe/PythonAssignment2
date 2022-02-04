9bec9b18fe3d6e933d4db78121f1c61da09cb07e
akashmothe <akash.mothe@ecolibriumenergy.com>
2022-02-04 14:16:28 +0530

    updated

--- problem7.py
+++ problem7.py
@@ -1,21 +1,34 @@
-# only applicable for n = 5
-# for i in range(1,6):
-#     print(11**i)
-
 
 def pascal_triangle(n):
-    res = [[1], [1,1]] 
+    res = [] # outer main list
+    # first getting required output as nested list
     for i in range(n):
-        tmp = [1]
-        for j in range(len(res[-1])):
-            tmp.append(res[-1][j]+res[-1][j+1])
-        tmp.append(1)
-
-        res.append(tmp)
+        row = [] # inner list
+        for j in range(i+1):
+            # first and last value is 1
+            # so getting it using j value and appending there as 1
+            if j == 0 or j == i:
+                row.append(1)
+            else:                                    
+                # accessing privious row by i-1 
+                # getting index of column by j value. 
+                # i.e by adding j'th index and privious value of j'th index  
+                row.append(res[i-1][j-1] + res[i-1][j]) 
+        res.append(row)
     return res
 
+# for printing above nested list in required shape
+def print_triangle(l):
+    for i in l:
+        for j in i:
+            print(j, end=' ')
+        print()
 
-print(pascal_triangle(3))
+n = int(input("Enter the number: "))
+print_triangle(pascal_triangle(n))
 
 
-# Tried but not getting required output
+# only applicable for n = 5
+
+# for i in range(1,6):
+#     print(11**i)
